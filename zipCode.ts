def44c72dca6b34e36869103472d410948bec941
kadsat <82785907+kadsat@users.noreply.github.com>
2023-04-28 21:01:12 -0400

    Update zipCode.ts

--- zipCode.ts
+++ zipCode.ts
@@ -1,13 +1,27 @@
-function isValidZipCd(): boolean {
-    const element = document.getElementById('_zip') as HTMLInputElement;
-    const zip_cd = element.value;
-  
-    if ((zip_cd.length === 5 || zip_cd.length === 9) && !!parseInt(zip_cd)) {
-      if (zip_cd.length === 9) {
-        return zip_cd.slice(5, 9) in { '0000': '', '9999': '' } ? false : true;
-      }
-      return true;
-    } else {
-      return false;
+function traverseComponent(component) {
+  // Base case: if the component has no children, return an empty object
+  if (!component.props.children) {
+    return {};
+  }
+
+  // Recursive case: traverse the child components and construct the output object
+  let outputObject = {};
+
+  React.Children.forEach(component.props.children, child => {
+    if (typeof child.type === "string") {
+      // If the child is an HTML element, skip it
+      return;
     }
-  }  
+
+    // Traverse the child component and add its state to the output object
+    const childOutput = traverseComponent(child);
+    outputObject[child.type.name] = childOutput;
+  });
+
+  // Add the component's own state to the output object
+  if (component.state) {
+    outputObject.state = component.state;
+  }
+
+  return outputObject;
+}
